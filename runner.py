76e9dd542bff3c77ed58bf8db32285b1c64591d6
Gabriele Fronzé <sucre.91@hotmail.it>
2021-07-08 18:37:36 +0200

    Additional examples

--- runner.py
+++ runner.py
@@ -3,14 +3,22 @@
 # BenchmarkSuite().startBenchmark()
 # from benchmarks.benchmark_suite.benchmark_suite import BenchmarkSuite
 # import typer
-from user_interfaces import cli
+# from user_interfaces import cli
 
 # from user_interfaces import cli
 # from benchmarks.dummy_regressor.implementation import DummyRegressor
 
+from common.benchmark_suite import BenchmarkSuite
+from common.benchmark_factory import BenchmarkFactory
+
 if __name__ == "__main__":
+    BenchmarkSuite(suite_info_path="./suites/example_suite.json").startBenchmark()
+    BenchmarkSuite(suite_info_path="./suites/example_suite_2.json").startBenchmark()
+    BenchmarkFactory(benchmark_name="dummy_benchmark", benchmark_settings_file="settings1.json").startBenchmark()
+    BenchmarkFactory(benchmark_name="dummy_benchmark", benchmark_settings_file="settings2.json").startBenchmark()
+    BenchmarkFactory(benchmark_name="dummy_benchmark", benchmark_settings_file="settings2.json").startBenchmark()
     # BenchmarkSuite().startBenchmark()
-    cli.main()
+    # cli.main()
     # InterfaceSkeleton().startBenchmark()
     # DummyRegressor().startBenchmark()
     # BenchmarkSuite().startBenchmark()
