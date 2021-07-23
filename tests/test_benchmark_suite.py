69215b83e8ed0132bb115bf04ffb25ba5bfb1678
aneesh404 <aneesh404@gmail.com>
2021-07-24 02:41:35 +0530

    Added tests for Benchmark Suite.

---
+++ tests/test_benchmark_suite.py
@@ -0,0 +1,24 @@
+import unittest
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from common.benchmark_wrapper import BenchmarkWrapper
+from common.benchmark_suite import BenchmarkSuite
+
+
+class TestArguments(unittest.TestCase):
+    def test_class(self):
+        self.assertEqual(issubclass(BenchmarkSuite, BenchmarkWrapper), True)
+
+    def test_ctor(self):
+        with self.assertRaises(FileNotFoundError) or self.assertRaises(Exception):
+            BenchmarkSuite("./wrongPath")
+
+    def test_startBenchmark(self):
+        with self.assertRaises(AttributeError):
+            BenchmarkSuite.startBenchmark("Random_Undefined_Argument")
+
+
+if __name__ == "__main__":
+    unittest.main()
