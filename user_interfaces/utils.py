506dd7f17d93e8ef7ad428ff6d234a7dcad43582
aneesh404 <aneesh404@gmail.com>
2021-08-20 00:04:37 +0530

    Flake8 formatting

--- user_interfaces/utils.py
+++ user_interfaces/utils.py
@@ -11,7 +11,7 @@ home_dir = Path.cwd()
 
 def isBenchmark(path):
     """
-        Checks to make sure given path belongs to a benchmark
+    Checks to make sure given path belongs to a benchmark
     """
     try:
         if "benchmark_info.json" in os.listdir(path) and os.path.isdir(path):
@@ -23,7 +23,7 @@ def isBenchmark(path):
 
 def getBenchmarksToRun():
     """
-        Gets the Runnable benchmark while also performing the isBenchmark check
+    Gets the Runnable benchmark while also performing the isBenchmark check
     """
     return [
         {"name": x}
@@ -34,12 +34,14 @@ def getBenchmarksToRun():
 
 def setItUp(benchmarkPath):
     """
-        Executes the setup file if present in benchmark directory
+    Executes the setup file if present in benchmark directory
     """
     if "setup.py" in os.listdir(benchmarkPath):
         try:
             process = subprocess.run(
-                [sys.executable, os.path.join(benchmarkPath, "setup.py")], check=True, universal_newlines=True
+                [sys.executable, os.path.join(benchmarkPath, "setup.py")],
+                check=True,
+                universal_newlines=True,
             )
         except subprocess.CalledProcessError as e:
             print(e.output)
@@ -55,14 +57,14 @@ def setItUp(benchmarkPath):
 
 def getSuitesToRun():
     """
-        Gets all the runnable suites
+    Gets all the runnable suites
     """
     return [{"name": x} for x in os.listdir(os.path.join(home_dir, "suites"))]
 
 
 def getSettings(bmark, runType):
     """
-        Gets a list of available settings
+    Gets a list of available settings
     """
     if runType == "Suite":
         return [
@@ -80,7 +82,7 @@ def getSettings(bmark, runType):
 
 def suiteMaker(suiteBuild: dict, suiteList: list):
     """
-        Builds up a suite according to given details
+    Builds up a suite according to given details
     """
     runnerDict = dict(
         {
@@ -89,16 +91,14 @@ def suiteMaker(suiteBuild: dict, suiteList: list):
             "benchmarks": suiteList,
         }
     )
-    suitePath = os.path.join(
-        home_dir, "suites", suiteBuild["FileName"] + ".json"
-    )
+    suitePath = os.path.join(home_dir, "suites", suiteBuild["FileName"] + ".json")
     with open(suitePath, "w") as configFile:
         json.dump(runnerDict, configFile, indent=4)
 
 
 def logIT(benchmark, logs, settings=None, pathToLog="./logs"):
     """
-        Logs the given list/dictionary
+    Logs the given list/dictionary
     """
     if logs is None:
         logs = "The Benchmark doesn't return any log"
@@ -106,17 +106,19 @@ def logIT(benchmark, logs, settings=None, pathToLog="./logs"):
     date = "".join(str(date).split("-"))
     time = "".join(str(time).split(":"))[:-7]
     if settings is not None:
-        path = Path.cwd().joinpath(pathToLog, benchmark, str(settings)[:-5], str(date) + '_' + str(time))
+        path = Path.cwd().joinpath(
+            pathToLog, benchmark, str(settings)[:-5], str(date) + "_" + str(time)
+        )
     else:
-        path = Path.cwd().joinpath(pathToLog, benchmark, str(date) + '_' + str(time))
+        path = Path.cwd().joinpath(pathToLog, benchmark, str(date) + "_" + str(time))
     path.mkdir(parents=True, exist_ok=True)
-    with open(os.path.join(path, 'output.log'), "a") as logFile:
+    with open(os.path.join(path, "output.log"), "a") as logFile:
         logFile.write(json.dumps(logs, indent=4))
 
 
 def tablify(legend, data, sorting=False, col=0):
     """
-        Creates a pretty table out of data provided
+    Creates a pretty table out of data provided
     """
     table = PrettyTable(legend)
     if sorting:
