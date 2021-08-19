edc3c478b58e172e83425172bbdd5fcea157696b
Gabriele Fronzé <sucre.91@hotmail.it>
2021-08-19 17:18:51 +0200

    Adding test script

---
+++ test_PR.sh
@@ -0,0 +1,20 @@
+
+#/bin.bash
+
+echo
+echo "INSTALLING DEPENDENCIES"
+pip install flake8 pytest
+if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
+
+echo
+echo "RUNNING FLAKE8"
+flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 . --count --max-complexity=10 --max-line-length=127 --statistics
+
+echo
+echo "RUNNING PYTEST"
+pytest
+
+echo
+echo "RUNNING BASH TESTS"
+# bash ./bin/test_*
