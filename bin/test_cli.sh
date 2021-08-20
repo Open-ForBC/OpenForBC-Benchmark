eed68e91b999bfef953a2cc2471a5956483672fe
leggerf <federica.legger@gmail.com>
2021-08-20 12:17:22 +0200

    refactor tests

--- bin/test_cli.sh
+++ bin/test_cli.sh
@@ -13,8 +13,14 @@ python3 user_interfaces/cli.py get-settings -b dummy_benchmark
 echo "python3 user_interfaces/cli.py run-benchmark -b dummy_benchmark -s settings1.json -v 1"
 python3 user_interfaces/cli.py run-benchmark -b dummy_benchmark -s settings1.json -v 1 
 
+echo "python3 user_interfaces/cli.py run-benchmark -b dummy_benchmark -s settings2.json -v 1"
+python3 user_interfaces/cli.py run-benchmark -b dummy_benchmark -s settings2.json -v 1
+
 echo "python3 user_interfaces/cli.py make-suite --name Mysuite -b dummy_benchmark -s settings1.json -f my_suite -d \"This is demo description.\""  
 python3 user_interfaces/cli.py make-suite --name Mysuite -b dummy_benchmark -s settings1.json -f my_suite -d "This is demo description."  
+
+echo "python3 user_interfaces/cli.py run-suite example_suite.json"
+python3 user_interfaces/cli.py run-suite example_suite.json
 
 echo "python3 user_interfaces/cli.py list-logs"
 python3 user_interfaces/cli.py list-logs
