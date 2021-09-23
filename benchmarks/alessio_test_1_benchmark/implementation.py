4d3ed89f557887ef08a1e3236746b16509bac685
Alessio <aleborri97@gmail.com>
2021-09-23 16:25:45 +0200

    Editing

--- benchmarks/alessio_test_1_benchmark/implementation.py
+++ benchmarks/alessio_test_1_benchmark/implementation.py
@@ -2,21 +2,27 @@ from common.benchmark_wrapper import BenchmarkWrapper
 import json
 import os
 
-class Alessio1Benchmark(benchmarkWrapper): """it's the same class inside benchmark_info.json"""
+class Alessio1Benchmark(benchmarkWrapper): #it's the same class inside benchmark_info.json
 
     def __init__(self):
-        self.filePath = os.path.dirname(__file__) """os.path.dirname(__file__) returns the path of this file"""
+        self.filePath = os.path.dirname(__file__) #os.path.dirname(__file__) returns the path of this file
         pass
     
 
     def setSettings(self, settings_file):
+        """
+        Open and read the settings file
+        """
         settings = os.path.join(self.filePath, "settings", setting_file)
+        self.dimension1 = json.load(open(settings, "r"))[dimension1]
+        self.dimension2 = json.load(open(settings, "r"))[dimension2]
 
     
     def startBenchmark(self, verbosity=None):
+        
+        print("The first dimension of the matrix is equal to", self.dimension1)
 
-
-        return {"output": """something to put out"""}
+        return {"output": #something to put out} #Where does this output go?
 
     
     def benchmarkStatus():
