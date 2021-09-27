24fc497af123a83a89aeb7eddb24072bbf16fd59
Alessio <aleborri97@gmail.com>
2021-09-27 15:30:16 +0200

    Editing

--- benchmarks/alessio_test_1_benchmark/implementation.py
+++ benchmarks/alessio_test_1_benchmark/implementation.py
@@ -2,6 +2,8 @@ from common.benchmark_wrapper import BenchmarkWrapper
 import json
 import os
 
+import torch 
+
 class Alessio1Benchmark(BenchmarkWrapper): #it's the same class inside benchmark_info.json
 
     def __init__(self):
@@ -21,9 +23,16 @@ class Alessio1Benchmark(BenchmarkWrapper): #it's the same class inside benchmark
     def startBenchmark(self, verbosity=None):
         
         print("The first dimension of the matrix is equal to", self.dimension1)
+        print("The second dimension of the matrix is equal to", self.dimension2)
 
+        matrix_1 = torch.randn(self.dimension1, self.dimension2)
+        matrix_2 = torch.randn(self.dimension1, self.dimension2)
+
+        product = torch.tensordot(matrix_1, matrix_2)
+
+        print("The shape of the product matrix is", product.shape())
     
-        return {"output": """something to put out"""} #Where does this output go?
+        return {"output": product} #Where does this output go?
 
     
     def benchmarkStatus():
