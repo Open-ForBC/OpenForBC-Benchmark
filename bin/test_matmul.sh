915b0467b39c9529fd386663cec98d10e12f22ef
AlessioBorriero <aleborri@gmail.com>
2021-11-11 09:39:08 +0100

    Add matmul test to CI.

--- bin/test_matmul.sh
+++ bin/test_matmul.sh
@@ -1,25 +1,5 @@
-echo "Running matmul benchmark settings with matrix dimensions 57x89 with CPU"
-python3 user_interfaces/cli.py run-benchmark -b matmul_benchmark -s Matrix_57x89_CPU.json -v 1 
-
-echo ""
 echo "Running matmul benchmark settings with matrix dimensions 57x89 with GPU"
 python3 user_interfaces/cli.py run-benchmark -b matmul_benchmark -s Matrix_57x89_GPU.json -v 1 
-
-echo ""
-echo "Running matmul benchmark settings with matrix dimensions 557x489 with CPU"
-python3 user_interfaces/cli.py run-benchmark -b matmul_benchmark -s Matrix_557x489_CPU.json -v 1
-
-echo ""
-echo "Running matmul benchmark settings with matrix dimensions 557x489 with GPU"
-python3 user_interfaces/cli.py run-benchmark -b matmul_benchmark -s Matrix_557x489_GPU.json -v 1 
-
-echo ""
-echo "Running matmul benchmark settings with matrix dimensions 700x800 with CPU"
-python3 user_interfaces/cli.py run-benchmark -b matmul_benchmark -s Matrix_700x800_CPU.json -v 1 
-
-echo ""
-echo "Running matmul benchmark settings with matrix dimensions 700x800 with GPU"
-python3 user_interfaces/cli.py run-benchmark -b matmul_benchmark -s Matrix_700x800_GPU.json -v 1 
 
 echo ""
 echo "list logs"
